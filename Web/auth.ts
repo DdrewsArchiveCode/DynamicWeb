4188fa5bda712101cec6fef52c851937b8b25546
Ddrewy <deandrew.y@gmail.com>
2025-03-17 21:56:31 +1100

    update: updating my branch

---
+++ Web/auth.ts
@@ -0,0 +1,7 @@
+import { user } from "./interface";
+
+function register(userName: string, email: string, password: string) {
+    return 0;
+}
+
+function login(email: string, password: string) {}
---
+++ Web/interface.ts
@@ -0,0 +1,10 @@
+export interface user {
+    userName: String,
+    password: String,
+    email: String,
+    UserId: Number
+}
+
+export interface Data {
+    users: user[]
+}
